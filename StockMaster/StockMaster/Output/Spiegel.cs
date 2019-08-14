094c2914eb1e3d38668c49ad4f4c5e9854360d76
Trawacho <sankturm@gmail.com>
2019-08-14 16:48:21 +0200

    Changing Spiegel.cs

--- StockMaster/StockMaster/Output/Spiegel.cs
+++ StockMaster/StockMaster/Output/Spiegel.cs
@@ -1,15 +1,11 @@
-﻿using StockMaster.BaseClasses;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Shapes;
+using StockMaster.BaseClasses;
 
 namespace StockMaster.Output
 {
