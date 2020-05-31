f8bf65528c9d8bc4ddd4d92c25a8f141c48bf659
Trawacho <sankturm@gmail.com>
2020-05-31 22:00:54 +0200

    update

--- StockMaster/StockMaster/BaseClasses/SerializableTournamentSet.cs
+++ StockMaster/StockMaster/BaseClasses/SerializableTournamentSet.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using StockMaster.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Linq;
 using System.Xml.Serialization;
 
